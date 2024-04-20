6d3bd484e7bfadf7b4b182567724ba03bdb10705
troovi <troovi@icloud.com>
2024-04-20 13:20:46 +0800

    update

--- pdf.ts
+++ pdf.ts
@@ -16,7 +16,10 @@ interface Sizes {
 export const makePDF = async (opts: Options) => {
   const { html, matchTarget, scale, landscape, format } = opts;
 
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
+
   const page = await browser.newPage();
 
   await page.setContent(html);
